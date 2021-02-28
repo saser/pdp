a87feb4db043ed80dccfc7b19c9bfee762c8dfca
Christian Persson <Saser@users.noreply.github.com>
2021-02-28 19:16:56 +0100

    tasks: split server into command and package
    This is more in line with current Go conventions of keeping commands
    in a `cmd` directory.

---
+++ tasks/server/server.go
@@ -0,0 +1,46 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+
+	taskspb "github.com/Saser/pdp/genproto/tasks/v1"
+)
+
+type Server struct {
+	taskspb.UnimplementedTasksServer
+
+	mu    sync.Mutex
+	tasks map[string]*taskspb.Task
+}
+
+func New() *Server {
+	return &Server{
+		tasks: make(map[string]*taskspb.Task),
+	}
+}
+
+func (s *Server) GetTask(ctx context.Context, req *taskspb.GetTaskRequest) (*taskspb.Task, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	task, ok := s.tasks[req.GetName()]
+	if !ok {
+		return nil, status.Errorf(codes.NotFound, "task not found: %q", req.GetName())
+	}
+	return task, nil
+}
+
+func (s *Server) CreateTask(ctx context.Context, req *taskspb.CreateTaskRequest) (*taskspb.Task, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	name := fmt.Sprintf("tasks/%d", len(s.tasks)+1)
+	task := proto.Clone(req.GetTask()).(*taskspb.Task)
+	task.Name = name
+	s.tasks[name] = task
+	return task, nil
+}
