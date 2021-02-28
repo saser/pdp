e194c355c72b742d1e9edfcdd1b8d1cb395f3e0f
Christian Persson <Saser@users.noreply.github.com>
2021-02-28 18:52:31 +0100

    tasks: implement `GetTask` and `CreateTask`
    Now I can interact with the server using a tool such as
    `grpcurl`. This is mostly for "proof of concept" purposes.

--- tasks/main.go
+++ tasks/main.go
@@ -3,24 +3,54 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"sync"
 
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
 
 	taskspb "github.com/Saser/pdp/genproto/tasks/v1"
 )
 
 type server struct {
 	taskspb.UnimplementedTasksServer
+
+	mu    sync.Mutex
+	tasks map[string]*taskspb.Task
 }
 
 func newServer() *server {
-	return &server{}
+	return &server{
+		tasks: make(map[string]*taskspb.Task),
+	}
+}
+
+func (s *server) GetTask(ctx context.Context, req *taskspb.GetTaskRequest) (*taskspb.Task, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	task, ok := s.tasks[req.GetName()]
+	if !ok {
+		return nil, status.Errorf(codes.NotFound, "task not found: %q", req.GetName())
+	}
+	return task, nil
+}
+
+func (s *server) CreateTask(ctx context.Context, req *taskspb.CreateTaskRequest) (*taskspb.Task, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	name := fmt.Sprintf("tasks/%d", len(s.tasks)+1)
+	task := proto.Clone(req.GetTask()).(*taskspb.Task)
+	task.Name = name
+	s.tasks[name] = task
+	return task, nil
 }
 
 func main() {
