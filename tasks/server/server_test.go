e53aac4d8c3ce79931e0556fa59f3c202a3e02ab
Christian Persson <Saser@users.noreply.github.com>
2021-03-05 22:11:48 +0100

    tasks/server: change test to use new grpctest.NewClientConnT function
    The register function ergonomics isn't super pleasant, but if I
    remember correctly from reading through the Go gRPC commits, this
    might change in the future by exposing a (*grpc.ServiceDesc) from the
    generated code.

--- tasks/server/server_test.go
+++ tasks/server/server_test.go
@@ -2,15 +2,13 @@ package server
 
 import (
 	"context"
-	"errors"
-	"net"
 	"strings"
 	"testing"
 
+	"github.com/Saser/pdp/testing/grpctest"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/test/bufconn"
 	"google.golang.org/protobuf/testing/protocmp"
 
 	taskspb "github.com/Saser/pdp/tasks/tasks_go_proto"
@@ -19,35 +17,8 @@ import (
 const bufSize = 1024 * 1024
 
 func setup(t *testing.T) taskspb.TasksClient {
-	lis := bufconn.Listen(bufSize)
-	t.Cleanup(func() {
-		if err := lis.Close(); err != nil && errors.Is(err, net.ErrClosed) {
-			t.Errorf("lis.Close() = %v; want nil", err)
-		}
-	})
-
-	gs := grpc.NewServer()
-	t.Cleanup(gs.GracefulStop)
-	s := New()
-	taskspb.RegisterTasksServer(gs, s)
-	go func() {
-		if err := gs.Serve(lis); err != nil {
-			t.Errorf("gs.Serve() = %v; want nil", err)
-		}
-	}()
-
-	dialer := func(context.Context, string) (net.Conn, error) {
-		return lis.Dial()
-	}
-	cc, err := grpc.Dial("bufconn", grpc.WithInsecure(), grpc.WithContextDialer(dialer))
-	if err != nil {
-		t.Fatalf("grpc.Dial() = %v; want nil", err)
-	}
-	t.Cleanup(func() {
-		if err := cc.Close(); err != nil {
-			t.Errorf("cc.Close() = %v; want nil", err)
-		}
-	})
+	register := func(s *grpc.Server) { taskspb.RegisterTasksServer(s, New()) }
+	cc := grpctest.NewClientConnT(t, register)
 	return taskspb.NewTasksClient(cc)
 }
 
