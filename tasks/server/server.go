cbc53855b4697e1393618804a7c81a7ff710ac55
Christian Persson <Saser@users.noreply.github.com>
2021-03-07 22:52:57 +0100

    tasks/server: remove current implementation and tests
    With the new list of test cases (see tasks/testcases.md), I figured I
    might as well throw out the existing tests -- they were only
    proof-of-concept, after all. The only test that remains is a test for
    the `setup` function, which will act as a starting point for writing
    the new tests.
    
    Also update external dependencies as appropriate.

--- tasks/server/server.go
+++ tasks/server/server.go
@@ -1,69 +1,13 @@
 package server
 
 import (
-	"context"
-	"fmt"
-	"sync"
-
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/proto"
-
 	taskspb "github.com/Saser/pdp/tasks/tasks_go_proto"
 )
 
 type Server struct {
 	taskspb.UnimplementedTasksServer
-
-	mu    sync.Mutex
-	tasks map[string]*taskspb.Task
 }
 
 func New() *Server {
-	return &Server{
-		tasks: make(map[string]*taskspb.Task),
-	}
-}
-
-func (s *Server) GetTask(ctx context.Context, req *taskspb.GetTaskRequest) (*taskspb.Task, error) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	task, ok := s.tasks[req.GetName()]
-	if !ok {
-		return nil, status.Errorf(codes.NotFound, "task not found: %q", req.GetName())
-	}
-	return task, nil
-}
-
-func (s *Server) CreateTask(ctx context.Context, req *taskspb.CreateTaskRequest) (*taskspb.Task, error) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	name := fmt.Sprintf("tasks/%d", len(s.tasks)+1)
-	task := proto.Clone(req.GetTask()).(*taskspb.Task)
-	task.Name = name
-	s.tasks[name] = task
-	return task, nil
-}
-
-func (s *Server) ListTasks(ctx context.Context, req *taskspb.ListTasksRequest) (*taskspb.ListTasksResponse, error) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	var tasks []*taskspb.Task
-	for _, t := range s.tasks {
-		tasks = append(tasks, t)
-	}
-	return &taskspb.ListTasksResponse{
-		Tasks: tasks,
-	}, nil
-}
-
-func (s *Server) DeleteTask(ctx context.Context, req *taskspb.DeleteTaskRequest) (*taskspb.Task, error) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	task, ok := s.tasks[req.GetName()]
-	if !ok {
-		return nil, status.Errorf(codes.NotFound, "task not found: %q", req.GetName())
-	}
-	task.Deleted = true
-	return task, nil
+	return &Server{}
 }
--- tasks/server/server_test.go
+++ tasks/server/server_test.go
@@ -1,122 +1,26 @@
 package server
 
 import (
-	"context"
-	"strings"
 	"testing"
 
 	"github.com/Saser/pdp/testing/grpctest"
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 	"google.golang.org/grpc"
-	"google.golang.org/protobuf/testing/protocmp"
 
 	taskspb "github.com/Saser/pdp/tasks/tasks_go_proto"
 )
-
-const bufSize = 1024 * 1024
 
 func setup(t *testing.T) taskspb.TasksClient {
 	t.Helper()
 	register := func(s *grpc.Server) { taskspb.RegisterTasksServer(s, New()) }
 	cc := grpctest.NewClientConnT(t, register)
+	t.Cleanup(func() {
+		if err := cc.Close(); err != nil {
+			t.Errorf("cc.Close() = %v; want nil", err)
+		}
+	})
 	return taskspb.NewTasksClient(cc)
 }
 
-func createTasks(ctx context.Context, t *testing.T, c taskspb.TasksClient, tasks []*taskspb.Task) []*taskspb.Task {
-	t.Helper()
-	created := make([]*taskspb.Task, len(tasks))
-	for i, task := range tasks {
-		req := &taskspb.CreateTaskRequest{
-			Task: task,
-		}
-		task2, err := c.CreateTask(ctx, req)
-		if err != nil {
-			t.Errorf("CreateTask(%v) err = %v; want nil", req, err)
-		}
-		created[i] = task2
-	}
-	t.Cleanup(func() {
-		for _, task := range created {
-			req := &taskspb.DeleteTaskRequest{
-				Name: task.GetName(),
-			}
-			deleted, err := c.DeleteTask(ctx, req)
-			if err != nil {
-				t.Errorf("DeleteTask(%v) err = %v; want nil", req, err)
-			}
-			if deleted.GetDeleted() == false {
-				t.Error("deleted.GetDeleted() = false; want true")
-			}
-		}
-	})
-	return created
-}
-
-func TestServer_CreateTask(t *testing.T) {
-	c := setup(t)
-	ctx := context.Background()
-	createReq := &taskspb.CreateTaskRequest{
-		Task: &taskspb.Task{
-			Title: "My task",
-		},
-	}
-	created, err := c.CreateTask(ctx, createReq)
-	if err != nil {
-		t.Errorf("c.CreateTask(%v) err = %v; want nil", createReq, err)
-	}
-	if got, prefix := created.GetName(), "tasks/"; !strings.HasPrefix(got, prefix) {
-		t.Errorf("created.GetName() = %q; want prefix %q", got, prefix)
-	}
-
-	getReq := &taskspb.GetTaskRequest{
-		Name: created.GetName(),
-	}
-	got, err := c.GetTask(ctx, getReq)
-	if err != nil {
-		t.Errorf("c.GetTask(%v) err = %v; want nil", getReq, err)
-	}
-	if diff := cmp.Diff(created, got, protocmp.Transform()); diff != "" {
-		t.Errorf("diff between CreateTask() and GetTask() (-want +got)\n%s", diff)
-	}
-}
-
-func TestServer_ListTasks(t *testing.T) {
-	c := setup(t)
-	ctx := context.Background()
-	tasks := createTasks(ctx, t, c, []*taskspb.Task{
-		{Title: "Some task"},
-		{Title: "Some other task"},
-	})
-	req := &taskspb.ListTasksRequest{}
-	res, err := c.ListTasks(ctx, req)
-	if err != nil {
-		t.Errorf("ListTasks(%v) err = %v; want nil", err)
-	}
-	less := func(t1, t2 *taskspb.Task) bool { return t1.GetName() < t2.GetName() }
-	if diff := cmp.Diff(tasks, res.GetTasks(), protocmp.Transform(), cmpopts.SortSlices(less)); diff != "" {
-		t.Errorf("diff between created tasks and ListTasks() (-want +got)\n%s", diff)
-	}
-}
-
-func TestServer_DeleteTask(t *testing.T) {
-	c := setup(t)
-	ctx := context.Background()
-	tasks := createTasks(ctx, t, c, []*taskspb.Task{
-		{Title: "Some task"},
-	})
-	task := tasks[0]
-	req := &taskspb.DeleteTaskRequest{
-		Name: task.GetName(),
-	}
-	deleted, err := c.DeleteTask(ctx, req)
-	if err != nil {
-		t.Errorf("DeleteTask(%v) err = %v; want nil", req, err)
-	}
-	if deleted.GetDeleted() == false {
-		t.Error("deleted.GetDeleted() = true; want false")
-	}
-	if diff := cmp.Diff(task, deleted, protocmp.Transform(), protocmp.IgnoreFields(task, "deleted")); diff != "" {
-		t.Errorf("diff between before and after deleted (-before +after)\n%s", diff)
-	}
+func Test_setup(t *testing.T) {
+	_ = setup(t)
 }
