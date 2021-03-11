94a9e7e86b12084e43eff5a82fd31805dcdeb78c
Christian Persson <Saser@users.noreply.github.com>
2021-03-11 23:17:12 +0100

    testing/grpctest: fix flake in NewClientConnT
    The bug was that the call to grpc.Dial did not include the
    grpc.WithBlock option. This sometimes caused tests that call
    NewClientConnT to finish before the gRPC server had even started
    up, meaning that s.GracefulStop was called before s.Serve had started
    up the server. This meant that s.Serve returned grpc.ErrServerStopped,
    and the goroutine running the gRPC server would call t.Errorf after
    the test had finished -- which results in a panic.
    
    While adding grpc.WithBlock to the call to grpc.Dial should be enough
    to fix this bug, I also took the opportunity to clean up the test some
    more -- notably cleaning up the *grpc.ClientConn, and running the gRPC
    server in an errgroup.Group (which is a great package and type).

--- testing/grpctest/grpctest.go
+++ testing/grpctest/grpctest.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"testing"
 
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/test/bufconn"
 )
@@ -13,6 +14,7 @@ const bufSize = 1024 * 1024
 
 func NewClientConnT(t testing.TB, register func(*grpc.Server)) *grpc.ClientConn {
 	t.Helper()
+
 	lis := bufconn.Listen(bufSize)
 	t.Cleanup(func() {
 		if err := lis.Close(); err != nil && err != net.ErrClosed {
@@ -21,19 +23,32 @@ func NewClientConnT(t testing.TB, register func(*grpc.Server)) *grpc.ClientConn 
 		}
 	})
 
+	var g errgroup.Group
+	t.Cleanup(func() {
+		if err := g.Wait(); err != nil {
+			t.Errorf("g.Wait() = %v; want nil", err)
+		}
+	})
+
 	s := grpc.NewServer()
 	register(s)
-	go func() {
-		if err := s.Serve(lis); err != nil {
-			t.Errorf("s.Serve() = %v; want nil", err)
-			return
-		}
-	}()
+	g.Go(func() error {
+		return s.Serve(lis)
+	})
 	t.Cleanup(s.GracefulStop)
-	dialer := func(context.Context, string) (net.Conn, error) { return lis.Dial() }
-	cc, err := grpc.Dial("bufconn", grpc.WithInsecure(), grpc.WithContextDialer(dialer))
+
+	dialer := func(context.Context, string) (net.Conn, error) {
+		return lis.Dial()
+	}
+	cc, err := grpc.Dial("bufconn", grpc.WithInsecure(), grpc.WithContextDialer(dialer), grpc.WithBlock())
 	if err != nil {
 		t.Fatalf("grpc.Dial() err = %v; want nil", err)
 	}
+	t.Cleanup(func() {
+		if err := cc.Close(); err != nil {
+			t.Errorf("cc.Close() = %v; want nil", err)
+		}
+	})
+
 	return cc
 }
