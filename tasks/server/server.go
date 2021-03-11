7a87a09b9bddd2ef5f9420d6d7cbcdd89f2ed7d1
Christian Persson <Saser@users.noreply.github.com>
2021-03-11 23:35:49 +0100

    tasks/server: hacky implementation of GetTask and CreateTask
    This is just to get something started, so that I can write some tests
    that do not fail multiple times with Unimplemented errors.

--- tasks/server/server.go
+++ tasks/server/server.go
@@ -1,13 +1,51 @@
 package server
 
 import (
+	"context"
+	"fmt"
+	"strings"
+	"sync"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	taskspb "github.com/Saser/pdp/tasks/tasks_go_proto"
 )
 
 type Server struct {
 	taskspb.UnimplementedTasksServer
+
+	mu    sync.Mutex
+	tasks map[string]*taskspb.Task
 }
 
 func New() *Server {
-	return &Server{}
+	return &Server{
+		tasks: make(map[string]*taskspb.Task),
+	}
+}
+
+func (s *Server) GetTask(ctx context.Context, req *taskspb.GetTaskRequest) (*taskspb.Task, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	name := req.GetName()
+	if !strings.HasPrefix(name, "tasks/") {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid task name: %q", name)
+	}
+	task, ok := s.tasks[name]
+	if !ok {
+		return nil, status.Errorf(codes.NotFound, "task not found: %q", name)
+	}
+	return task, nil
+}
+
+func (s *Server) CreateTask(ctx context.Context, req *taskspb.CreateTaskRequest) (*taskspb.Task, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	task := req.GetTask()
+	task.Name = fmt.Sprintf("tasks/%d", len(s.tasks)+1)
+	task.Deleted = false
+	task.Completed = false
+	s.tasks[task.Name] = task
+	return task, nil
 }
