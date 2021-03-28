c6c9278209a9acf75d44efa90c9cd51934ac7aa8
Christian Persson <Saser@users.noreply.github.com>
2021-03-28 22:31:41 +0200

    Remove Bazel build files and generate Go code manually
    I'm really beginning to feel that using Bazel is turning out to be
    more trouble than it's worth, at least for now. Just using manual code
    generation and the `go` tools is definitely good enough for now. As a
    bonus, now `gopls` will be able to work with the protos.
    
    In the future I might bring Bazel back, e.g., when it's time to begin
    to build multi-language stuff (say, a JS frontend).

---
+++ Makefile
@@ -0,0 +1,23 @@
+include tools.mk
+
+proto_files := $(shell git ls-files -- '*.proto')
+go_module := $(shell go list -m)
+
+.PHONY: generate
+generate: \
+	$(proto_files) \
+	$(protoc) \
+	$(protoc-gen-go) \
+	$(protoc-gen-go-grpc) \
+	$(protoc_dir)
+generate:
+	$(protoc) \
+		--proto_path='.' \
+		--proto_path='$(protoc_dir)/include' \
+		--plugin='$(protoc-gen-go)' \
+		--go_out=. \
+		--go_opt=module='$(go_module)' \
+		--plugin='$(protoc-gen-go-grpc)' \
+		--go-grpc_out=. \
+		--go-grpc_opt=module='$(go_module)' \
+		$(proto_files)
---
+++ tools.mk
@@ -0,0 +1,43 @@
+# tools.mk: rules for installing tools used by this project.
+
+tools := tools
+$(tools):
+	mkdir -p '$@'
+
+# protoc: the protobuf compiler
+protoc_version := 3.15.6
+protoc_dir := $(tools)/protoc-$(protoc_version)
+protoc_archive := $(protoc_dir).zip
+protoc := $(protoc_dir)/bin/protoc
+# TODO: make this more platform-independent (`osx` is specified in the archive URL.)
+$(protoc_archive): | $(tools)
+	curl \
+		--fail \
+		--location \
+		--show-error \
+		--silent \
+		--output '$@' \
+		'https://github.com/protocolbuffers/protobuf/releases/download/v$(protoc_version)/protoc-$(protoc_version)-osx-x86_64.zip'
+
+$(protoc_dir): $(protoc_archive)
+	unzip \
+		'$<' \
+		-d '$@'
+
+$(protoc): | $(protoc_dir)
+
+# protoc-gen-go: protoc plugin to generate Go code for protobufs.
+protoc-gen-go := $(tools)/protoc-gen-go
+$(protoc-gen-go): go.mod go.sum
+	go \
+		build \
+		-o='$@' \
+		google.golang.org/protobuf/cmd/protoc-gen-go
+
+# protoc-gen-go-grpc: protoc plugin to generate Go code for protobuf services.
+protoc-gen-go-grpc := $(tools)/protoc-gen-go-grpc
+$(protoc-gen-go-grpc): go.mod go.sum
+	go \
+		build \
+		-o='$@' \
+		google.golang.org/grpc/cmd/protoc-gen-go-grpc
