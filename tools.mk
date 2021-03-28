2ae74f01713f462fc7365afcb444418d2efae7ec
Christian Persson <Saser@users.noreply.github.com>
2021-03-28 23:16:31 +0200

    tools.mk: add the `googleapis` repository as a tool dependency
    It's not really a tool, but it will be used only as part of code
    generation, which I consider to be tool-only.

--- tools.mk
+++ tools.mk
@@ -43,3 +43,24 @@ $(protoc-gen-go-grpc): go.mod go.sum
 		build \
 		-o='$@' \
 		google.golang.org/grpc/cmd/protoc-gen-go-grpc
+
+# googleapis: a repository of protobuf definitions, for example the useful `google.api` and
+# `google.type` packages. It's not a tool per se, but I'm hacking it in this way for now.
+#
+# branch "master" as of 2021-03-28
+googleapis_version := c3918feb2415878ac428d728fb473ed4187b7819
+googleapis_dir := $(tools)/googleapis-$(googleapis_version)
+googleapis_archive := $(googleapis_dir).zip
+$(googleapis_archive): | $(tools)
+	curl \
+		--fail \
+		--location \
+		--show-error \
+		--silent \
+		--output '$@' \
+		'https://github.com/googleapis/googleapis/archive/$(googleapis_version).zip'
+
+$(googleapis_dir): | $(googleapis_archive)
+	unzip \
+		'$(googleapis_archive)' \
+		-d '$(@D)'
