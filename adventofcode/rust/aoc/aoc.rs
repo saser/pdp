c5802011ce0865b73d9fe94318008a2b3d174b83
Christian Persson <Saser@users.noreply.github.com>
2021-06-13 16:52:10 +0200

    adventofcode/rust: move base stuff to `aoc` library
    The `base` library didn't cover everything I thought it covered, so I
    figured that I just put everything into an `aoc` library instead of
    messing around with modules and whatnot.
    
    This commit also updates all dependencies, as well as moves the `grid`
    library up one directory level.

---
+++ adventofcode/rust/aoc/aoc.rs
@@ -0,0 +1,60 @@
+use std::fmt;
+use std::io;
+use std::str::FromStr;
+
+pub type Solution = fn(r: &mut dyn io::Read) -> Result<String, String>;
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum Part {
+    One,
+    Two,
+}
+
+impl FromStr for Part {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "1" => Ok(Part::One),
+            "2" => Ok(Part::Two),
+            _ => Err(format!("part must be 1 or 2; was: {}", s)),
+        }
+    }
+}
+
+impl fmt::Display for Part {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = match *self {
+            Part::One => "1",
+            Part::Two => "2",
+        };
+        write!(f, "{}", s)
+    }
+}
+
+#[macro_export]
+macro_rules! test {
+    ($name:ident, $input:expr, $output:expr, $solution:expr) => {
+        #[test]
+        fn $name() {
+            let input: &str = $input;
+            let output: &str = $output;
+            let solution: adventofcode_rust_aoc::Solution = $solution;
+            assert_eq!(output, solution(&mut Box::new(input.as_bytes())).unwrap());
+        }
+    };
+    ($name:ident, file $file:expr, $output:expr, $solution:expr) => {
+        test!($name, include_str!($file), $output, $solution);
+    };
+    ($name:ident, $input:expr, file $file:expr, $solution:expr) => {
+        test!($name, $input, include_str!($file), $solution);
+    };
+    ($name:ident, file $infile:expr, file $outfile:expr, $solution:expr) => {
+        test!(
+            $name,
+            include_str!($infile),
+            include_str!($outfile),
+            $solution
+        );
+    };
+}
