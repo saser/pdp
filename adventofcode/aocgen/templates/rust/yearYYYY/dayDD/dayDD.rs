8473423903b159377694a2e3f0318670fb9d7a51
Christian Persson <Saser@users.noreply.github.com>
2021-06-13 18:12:05 +0200

    adventofcode/aocgen/templates/rust: update templates for Bazel

---
+++ adventofcode/aocgen/templates/rust/yearYYYY/dayDD/dayDD.rs
@@ -0,0 +1,16 @@
+use adventofcode_rust_aoc as aoc;
+
+pub fn part1(input: &str) -> Result<String, String> {
+    solve(input, aoc::Part::One)
+}
+
+pub fn part2(input: &str) -> Result<String, String> {
+    solve(input, aoc::Part::Two)
+}
+
+fn solve(input: &str, part: aoc::Part) -> Result<String, String> {
+    Err(format!("no implementation for part {}", part))
+}
+
+#[cfg(test)]
+mod {{.FullDay}}_test;
---
+++ adventofcode/aocgen/templates/rust/yearYYYY/dayDD/dayDD_test.rs
@@ -0,0 +1,11 @@
+use adventofcode_rust_aoc as aoc;
+
+mod part1 {
+    use super::*;
+    aoc::testfn!(actual, aoc::Input::File("adventofcode/inputs/{{.Year}}/{{.PaddedDay}}"), "TODO: fill in answer here", crate::part1);
+}
+
+mod part2 {
+    use super::*;
+    aoc::testfn!(actual, aoc::Input::File("adventofcode/inputs/{{.Year}}/{{.PaddedDay}}"), "TODO: fill in answer here", crate::part2);
+}
