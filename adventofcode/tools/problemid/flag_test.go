29d3ae85fcc7447c886cb962572b3ad0ff83b7fc
Christian Persson <Saser@users.noreply.github.com>
2021-07-15 08:32:23 +0200

    adventofcode/tools/problemid: use package cmp for testing

--- adventofcode/tools/problemid/flag_test.go
+++ adventofcode/tools/problemid/flag_test.go
@@ -4,23 +4,25 @@ import (
 	"flag"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/testing/protocmp"
+
 	adventofcodepb "github.com/Saser/pdp/adventofcode/adventofcode_go_proto"
 )
 
 func TestFlag(t *testing.T) {
 	fs := flag.NewFlagSet("test", flag.ContinueOnError)
-	id := Flag(fs, "problem_id", &adventofcodepb.ProblemID{}, "Problem ID")
+	got := Flag(fs, "problem_id", &adventofcodepb.ProblemID{}, "Problem ID")
 	arguments := []string{"-problem_id=year: 2015  day: 10  part: 1"}
 	if err := fs.Parse(arguments); err != nil {
 		t.Fatalf("fs.Parse(%q) = %v; want nil", arguments, err)
 	}
-	if got, want := id.GetYear(), int32(2015); got != want {
-		t.Errorf("id.GetYear() = %v; want %v", got, want)
+	want := &adventofcodepb.ProblemID{
+		Year: 2015,
+		Day:  10,
+		Part: adventofcodepb.ProblemID_ONE,
 	}
-	if got, want := id.GetDay(), int32(10); got != want {
-		t.Errorf("id.GetDay() = %v; want %v", got, want)
-	}
-	if got, want := id.GetPart(), adventofcodepb.ProblemID_ONE; got != want {
-		t.Errorf("id.GetDay() = %v; want %v", got, want)
+	if diff := cmp.Diff(want, got, protocmp.Transform()); diff != "" {
+		t.Errorf("unexpected result of flag parsing (-want +got)\n%s", diff)
 	}
 }
