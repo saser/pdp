f66f8c3c94b52357027e5b3c1293d57cf98e882a
Christian Persson <Saser@users.noreply.github.com>
2021-07-24 18:04:30 +0200

    adventofcode/tools/aoctool/cmd: simplify root command

--- adventofcode/tools/aoctool/cmd/root.go
+++ adventofcode/tools/aoctool/cmd/root.go
@@ -2,13 +2,7 @@ package cmd
 
 import "github.com/spf13/cobra"
 
-var (
-	rootCmd = &cobra.Command{
-		Use:   "aoctool",
-		Short: "A tool for working with Advent of Code solutions",
-		Long:  "Aoctool is a tool for interacting with the Advent of Code website (https://adventofcode.com) and with the solutions stored in this repository.",
-	}
-)
+var rootCmd = &cobra.Command{Use: "aoctool"}
 
 func Execute() error {
 	return rootCmd.Execute()
