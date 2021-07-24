360b648c026d7501049ef1fd41029ec94171c6f9
Christian Persson <Saser@users.noreply.github.com>
2021-07-24 19:52:41 +0200

    adventofcode/tools/aoctool: implement `fetch answer` command

--- adventofcode/tools/aoctool/cmd/fetch/answer/answer.go
+++ adventofcode/tools/aoctool/cmd/fetch/answer/answer.go
@@ -1,8 +1,14 @@
 package answer
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"os"
+	"os/signal"
+	"regexp"
 
+	"github.com/Saser/pdp/adventofcode/tools/aoctool/cmd/fetch/client"
 	"github.com/spf13/cobra"
 )
 
@@ -10,12 +16,15 @@ var (
 	cmd = &cobra.Command{
 		Use:   "answer",
 		Short: "Download the answer to a problem from the Advent of Code website.",
+		Long:  `Fetches the problem description page from the Advent of Code website, and uses an ugly hack based on a regex to try and parse out the answer from the HTML. The answer, if found, is printed to stdout.`,
 		RunE:  runE,
 	}
 
 	year int
 	day  int
 	part int
+
+	answerRE = regexp.MustCompile(`Your puzzle answer was \<code\>(.+?)\</code\>`)
 )
 
 func init() {
@@ -32,10 +41,46 @@ func Cmd() *cobra.Command {
 }
 
 func runE(cmd *cobra.Command, args []string) error {
+	if year < 2015 || year > 2020 {
+		return fmt.Errorf("--year=%d is outside range [2015, 2020]", year)
+	}
+	if day < 1 || day > 25 {
+		return fmt.Errorf("--day=%d is outside range [1, 25]", day)
+	}
+	if part < 1 || part > 2 {
+		return fmt.Errorf("--part=%d is not one of 1 or 2", part)
+	}
+	if day == 25 && part == 2 {
+		return errors.New("there is no part 2 for day 25")
+	}
 	session, err := cmd.Flags().GetString("session")
 	if err != nil {
 		return err
 	}
-	fmt.Printf("fetch answer: year=%d, day=%d, part=%d (session=%q)\n", year, day, part, session)
+
+	c, err := client.New(session)
+	if err != nil {
+		return err
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+	page, err := c.GetPage(ctx, year, day)
+	if err != nil {
+		return err
+	}
+	answers := parseAnswers(page)
+	if len(answers) < part {
+		return fmt.Errorf("no answer found for year %d, day %d, part %d", year, day, part)
+	}
+	fmt.Print(answers[part-1])
 	return nil
+}
+
+func parseAnswers(body string) []string {
+	var answers []string
+	for _, matches := range answerRE.FindAllStringSubmatch(body, -1) {
+		answers = append(answers, matches[1])
+	}
+	return answers
 }
