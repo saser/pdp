b7e96466bb4d1eeefb89593e17fbb64ab85aee4e
Christian Persson <Saser@users.noreply.github.com>
2021-08-07 15:17:12 +0200

    adventofcode/data: invoke `generate_instances` for 2015/24

--- adventofcode/go/year2015/day24/day24_test.go
+++ adventofcode/go/year2015/day24/day24_test.go
@@ -7,8 +7,7 @@ import (
 )
 
 var (
-	exampleFile = testcase.Runfile("adventofcode/go/year2015/day24/testdata/example")
-	inputFile   = testcase.Runfile("adventofcode/data/year2015/day24/actual.in")
+	inputFile = testcase.Runfile("adventofcode/data/year2015/day24/actual.in")
 
 	tcPart1 = testcase.NewFile("input", inputFile, "11266889531")
 	tcPart2 = testcase.NewFile("input", inputFile, "77387711")
@@ -16,7 +15,6 @@ var (
 
 func TestPart1(t *testing.T) {
 	for _, tc := range []testcase.TestCase{
-		testcase.NewFile(exampleFile, exampleFile, "99"),
 		tcPart1,
 	} {
 		tc.Test(t, Part1)
@@ -29,7 +27,6 @@ func BenchmarkPart1(b *testing.B) {
 
 func TestPart2(t *testing.T) {
 	for _, tc := range []testcase.TestCase{
-		testcase.NewFile(exampleFile, exampleFile, "44"),
 		tcPart2,
 	} {
 		tc.Test(t, Part2)
