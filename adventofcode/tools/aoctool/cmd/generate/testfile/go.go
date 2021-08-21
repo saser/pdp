00e14793e609a555d6f0b41037f3f3b721ffc693
Christian Persson <Saser@users.noreply.github.com>
2021-08-21 18:15:50 +0200

    adventofcode/tools/aoctool: implement Go code generation in `generate testfile`

---
+++ adventofcode/tools/aoctool/cmd/generate/testfile/go.go
@@ -0,0 +1,52 @@
+package testfile
+
+import (
+	"bytes"
+	"go/format"
+	"text/template"
+
+	_ "embed" // for embedding Go template
+)
+
+var (
+	//go:embed go.tmpl
+	goTmplText string
+	goTmpl     = template.Must(template.New("go.tmpl").Parse(goTmplText))
+
+	goOut  string
+	goData goTmplData
+)
+
+func init() {
+	cmd.Flags().StringVar(&goOut, "go_out", "", "Path to file where Go code should be written.")
+	cmd.Flags().StringVar(&goData.Package, "go_package", "", "Name of Go package to generate code in. Required if --go_out is non-empty.")
+	cmd.Flags().StringVar(&goData.Part1, "go_part1", "", "Name of solution function for part 1. Required if --go_out is non-empty.")
+	cmd.Flags().StringVar(&goData.Part2, "go_part2", "", "Name of solution function for part 2. Required if --go_out is non-empty.")
+}
+
+// goTmplData holds the data for the Go template.
+type goTmplData struct {
+	// Package is the name of the package the test should be
+	// embedded in. Example: "day01".
+	Package string
+	// Part1Blobs and Part2Blobs are the Instance messages
+	// marshalled in binary format for part 1 and part 2,
+	// respectively.
+	Part1Blobs, Part2Blobs [][]byte
+	// Part1 and Part2 are the names of the solution functions for
+	// part 1 and part 2, respectively. They should have the
+	// signature func(input string) (string, error).
+	Part1, Part2 string
+}
+
+func (d goTmplData) render() ([]byte, error) {
+	var buf bytes.Buffer
+	if err := goTmpl.Execute(&buf, d); err != nil {
+		return nil, err
+	}
+	src, err := format.Source(buf.Bytes())
+	if err != nil {
+		return nil, err
+	}
+	return src, nil
+}
--- adventofcode/tools/aoctool/cmd/generate/testfile/testfile.go
+++ adventofcode/tools/aoctool/cmd/generate/testfile/testfile.go
@@ -2,8 +2,14 @@ package testfile
 
 import (
 	"errors"
+	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	"google.golang.org/protobuf/encoding/prototext"
+	"google.golang.org/protobuf/proto"
+
+	adventofcodepb "github.com/Saser/pdp/adventofcode/adventofcode_go_proto"
 )
 
 var (
@@ -19,5 +25,58 @@ func Cmd() *cobra.Command {
 }
 
 func runE(cmd *cobra.Command, args []string) error {
-	return errors.New("unimplemented")
+	if len(args) == 0 {
+		return errors.New("at least one textproto file must be given as argument")
+	}
+	var part1Instances, part2Instances []*adventofcodepb.Instance
+	for _, arg := range args {
+		b, err := os.ReadFile(arg)
+		if err != nil {
+			return err
+		}
+		instance := &adventofcodepb.Instance{}
+		if err := prototext.Unmarshal(b, instance); err != nil {
+			return fmt.Errorf("read %q: %w", arg, err)
+		}
+		switch instance.GetProblem().GetPart() {
+		case 1:
+			part1Instances = append(part1Instances, instance)
+		case 2:
+			part2Instances = append(part2Instances, instance)
+		}
+	}
+
+	if goOut != "" {
+		if goData.Package == "" {
+			return errors.New("--go_package is required if --go_out is non-empty")
+		}
+		if goData.Part1 == "" {
+			return errors.New("--go_part1 is required if --go_out is non-empty")
+		}
+		if goData.Part2 == "" {
+			return errors.New("--go_part2 is required if --go_out is non-empty")
+		}
+		for _, instance := range part1Instances {
+			b, err := proto.Marshal(instance)
+			if err != nil {
+				return err
+			}
+			goData.Part1Blobs = append(goData.Part1Blobs, b)
+		}
+		for _, instance := range part2Instances {
+			b, err := proto.Marshal(instance)
+			if err != nil {
+				return err
+			}
+			goData.Part2Blobs = append(goData.Part2Blobs, b)
+		}
+		src, err := goData.render()
+		if err != nil {
+			return fmt.Errorf("render Go code: %w", err)
+		}
+		if err := os.WriteFile(goOut, src, 0644); err != nil {
+			return fmt.Errorf("render Go code: %w", err)
+		}
+	}
+	return nil
 }
