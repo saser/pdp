06461241b69ce2f6fe684bda8310d698d4e59db8
Christian Persson <Saser@users.noreply.github.com>
2021-09-09 21:48:21 +0200

    tasks3/server: add simple binary to run the service

---
+++ tasks3/server/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"github.com/Saser/pdp/tasks3/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	tasks3pb "github.com/Saser/pdp/tasks3/tasks3_go_proto"
+)
+
+var (
+	port = flag.Int("port", 8080, "The port to listen on.")
+)
+
+func emain() error {
+	flag.Parse()
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	svc := service.NewServer()
+	srv := grpc.NewServer()
+	reflection.Register(srv)
+	tasks3pb.RegisterTasksServer(srv, svc)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		return err
+	}
+	defer lis.Close()
+
+	errc := make(chan error, 1)
+	go func() {
+		errc <- srv.Serve(lis)
+	}()
+	<-ctx.Done()
+	srv.GracefulStop()
+	return <-errc
+}
+
+func main() {
+	if err := emain(); err != nil {
+		log.Fatal(err)
+	}
+}
