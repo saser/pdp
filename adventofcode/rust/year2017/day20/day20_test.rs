4141615aab3e8810af3c7f62439c4e6a7967f628
Christian Persson <Saser@users.noreply.github.com>
2021-11-07 19:27:32 +0000

    adventofcode/rust: add empty dir for 2017/20

---
+++ adventofcode/rust/year2017/day20/day20_test.rs
@@ -0,0 +1,21 @@
+use adventofcode_rust_aoc as aoc;
+
+mod part1 {
+    use super::*;
+    aoc::testfn!(
+        actual,
+        aoc::Data::File("adventofcode/data/year2017/day20/actual.in"),
+        aoc::Data::String("TODO: fill in answer here"),
+        crate::part1
+    );
+}
+
+mod part2 {
+    use super::*;
+    aoc::testfn!(
+        actual,
+        aoc::Data::File("adventofcode/data/year2017/day20/actual.in"),
+        aoc::Data::String("TODO: fill in answer here"),
+        crate::part2
+    );
+}
