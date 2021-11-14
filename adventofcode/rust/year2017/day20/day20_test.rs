2db7f0329e6a1c8368dc4ee3df61fcfb32f99a98
Christian Persson <Saser@users.noreply.github.com>
2021-11-14 21:08:20 +0000

    adventofcode/rust: 2017/20: comment out broken test for part 2

--- adventofcode/rust/year2017/day20/day20_test.rs
+++ adventofcode/rust/year2017/day20/day20_test.rs
@@ -10,12 +10,12 @@ mod part1 {
     );
 }
 
-mod part2 {
-    use super::*;
-    aoc::testfn!(
-        actual,
-        aoc::Data::File("adventofcode/data/year2017/day20/actual.in"),
-        aoc::Data::String("TODO: fill in answer here"),
-        crate::part2
-    );
-}
+// mod part2 {
+//     use super::*;
+//     aoc::testfn!(
+//         actual,
+//         aoc::Data::File("adventofcode/data/year2017/day20/actual.in"),
+//         aoc::Data::String("TODO: fill in answer here"),
+//         crate::part2
+//     );
+// }
