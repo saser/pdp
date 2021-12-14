c5dc25bf3c428dc05ec4942187427473c7d9596b
Christian Persson <Saser@users.noreply.github.com>
2021-12-14 13:04:01 +0000

    adventofcode/go: 2021/13: use point-set-based solution
    This improves the runtime quite a bit:
    
        name             old time/op  new time/op  delta
        Part1/actual-16  3.97ms ± 4%  0.25ms ±12%  -93.74%  (p=0.000 n=10+10)
        Part2/actual-16  7.10ms ± 4%  0.50ms ± 9%  -93.00%  (p=0.000 n=10+10)
    
    This solution also assumes that there are never any negative x or y values. See
    the comment in day13.go for why.

--- adventofcode/go/year2021/day13/day13.go
+++ adventofcode/go/year2021/day13/day13.go
@@ -34,7 +34,7 @@ func solve(input string, part int) (string, error) {
 		g.Fold(i)
 	}
 	if part == 1 {
-		return fmt.Sprint(g.ActiveCount()), nil
+		return fmt.Sprint(g.Len()), nil
 	}
 	return g.String(), nil
 }
@@ -62,103 +62,60 @@ func parseInstructions(s string) ([]instruction, error) {
 }
 
 type grid struct {
-	dots       []bool // laid out row-wise
-	rows, cols int
+	points map[geo.Point]bool
 }
 
 func parseGrid(s string) (*grid, error) {
-	var ps []geo.Point
-	xmax := -1
-	ymax := -1
+	points := make(map[geo.Point]bool)
 	for _, line := range strings.Split(s, "\n") {
 		parts := strings.Split(line, ",")
 		x, err := strconv.Atoi(parts[0])
 		if err != nil {
 			return nil, fmt.Errorf("parse grid: %w", err)
 		}
-		if x > xmax {
-			xmax = x
-		}
 		y, err := strconv.Atoi(parts[1])
-		if y > ymax {
-			ymax = y
-		}
 		if err != nil {
 			return nil, fmt.Errorf("parse grid: %w", err)
 		}
-		ps = append(ps, geo.Point{
-			X: x,
-			Y: y,
-		})
+		points[geo.Point{X: x, Y: y}] = true
 	}
-	// xmax and ymax are one less than the number of columns and rows,
-	// respectively.
-	// Example: if xmax = 4, then the largest x index is 4, which means 5
-	// columns.
-	cols := xmax + 1
-	rows := ymax + 1
-	dots := make([]bool, rows*cols)
-	g := &grid{
-		dots: dots,
-		rows: rows,
-		cols: cols,
-	}
-	for _, p := range ps {
-		i, _ := g.index(p.X, p.Y)
-		g.dots[i] = true
-	}
-	return g, nil
+	return &grid{
+		points: points,
+	}, nil
 }
 
-func (g *grid) index(x, y int) (int, bool) {
-	if x < 0 || x >= g.cols || y < 0 || y >= g.rows {
-		return 0, false
-	}
-	return y*g.cols + x, true
-}
-
-func (g *grid) Get(x, y int) (value bool, ok bool) {
-	i, ok := g.index(x, y)
-	if !ok {
-		return false, false
-	}
-	return g.dots[i], ok
-}
-
-func (g *grid) Set(x, y int, value bool) bool {
-	i, ok := g.index(x, y)
-	if !ok {
-		return false
-	}
-	g.dots[i] = value
-	return true
-}
-
-func (g *grid) ActiveCount() int {
-	n := 0
-	for _, b := range g.dots {
-		if b {
-			n++
-		}
-	}
-	return n
+func (g *grid) Len() int {
+	return len(g.points)
 }
 
 func (g *grid) String() string {
+	var xs, ys []int
+	for p := range g.points {
+		xs = append(xs, p.X)
+		ys = append(ys, p.Y)
+	}
+	maxX := intmath.Max(xs[0], xs[1:]...)
+	maxY := intmath.Max(ys[0], ys[1:]...)
 	var ss []string
-	for y := 0; y < g.rows; y++ {
+	for y := 0; y <= maxY; y++ {
 		var sb strings.Builder
-		for x := 0; x < g.cols; x++ {
-			r := '.'
-			if v, _ := g.Get(x, y); v {
-				r = '#'
+		for x := 0; x <= maxX; x++ {
+			if g.points[geo.Point{X: x, Y: y}] {
+				sb.WriteRune('#')
+			} else {
+				sb.WriteRune('.')
 			}
-			sb.WriteRune(r)
 		}
 		ss = append(ss, sb.String())
 	}
 	return strings.Join(ss, "\n")
 }
+
+// It is assumed in Fold, foldX, and foldY, that we never do folds that result
+// in negative x or y values. Why? The instructions are not entirely clear on
+// what would happen if that were the case. They do not specifically state that
+// folds only ever result in positive values, but seeing as all the instructions
+// only are for non-negative values, it seems like a reasonable assumption.
 
 func (g *grid) Fold(i instruction) {
 	if i.Along == "x" {
@@ -168,240 +125,33 @@ func (g *grid) Fold(i instruction) {
 	}
 }
 
-// The code for folding is a little unintuitive, so this is an summary of how it
-// works, with examples to help illustrate. The examples will be for folding to
-// the left ("along x") but the same principles apply for folding up ("along
-// y").
-//
-// Example 1: 7 columns, fold along x=4. There is only a two rows in this
-// example, to make it a bit smaller.
-//
-//     #...|.#
-//     #..#|#.
-//
-// Folding along a line splits the grid in two parts: one unchanged part and one
-// flipped part. The unchanged part is the one that ends up "below" the flipped
-// part in the fold.
-//
-//     #... | .#
-//     #..# | #.
-//     ^^^^   ^^
-//     ||||   || flipped part
-//     |||| unchanged part
-//
-// The flipped part is reversed in place.
-//
-//     #... | .#  becomes  #... | #.
-//     #..# | #.           #..# | .#
-//
-// The overlapping columns in the unchanged part and the flipped part are ORed
-// together and stored in a new, empty grid with the right size. That is done by
-// iterating over the number of overlapping columns, which is calculated as the
-// minimum of "number of columns in unchanged part" and "number of columns in
-// flipped part". In this case, that number is 2. Starting from the folding
-// line, the iteration starts 2 columns to the left, and 1 column to the right.
-// Then the iteration happens in lockstep.
-//
-// First loop of iteration:
-//
-//     #.[.]. | [#].  becomes  ..[#].
-//     #.[.]# | [.]#           ..[.].
-//     old grid                new grid
-//
-// Second loop of iteration:
-//
-//     #..[.] | #[.]  becomes  ..#[.]
-//     #..[#] | .[#]           ...[#]
-//     old grid                new grid
-//
-// What remains is filling in the initial part of the new grid. In this case,
-// the unchanged part is larger than the flipped part, so the values are taken
-// from the unchanged part. If the flipped part was larger, the values would be
-// taken from the flipped part.
-//
-// First loop of iteration:
-//
-//     [#]...|#.  becomes  [#].#.
-//     [#]..#|.#           [#]..#
-//     old grid            new grid
-//
-// Second loop of iteration:
-//
-//     #[.]..|#.  becomes  #[.]#.
-//     #[.].#|.#           #[.].#
-//     old grid            new grid
-//
-// The result of the fold is then:
-//
-//     #.#.
-//     #..#
-//
-// ----
-//
-// Example 2: 7 columns, fold along n=2.
-//
-//     #.|...#
-//     #.|#.#.
-//
-// Reverse the flipped part.
-//
-//     #.|...#  becomes  #.|#...
-//     #.|#.#.           #.|.#.#
-//
-// OR together the overlapping part, store in a new grid. Two things to note:
-//     * the iteration starts 2 steps from the end of the flipped part, not 1
-//       step to the right of the line (in the previous example, those were the
-//       same thing).
-//     * the result is again stored in the end of the new grid.
-//
-// First loop of iteration:
-//
-//     [#]. | #.[.].  becomes  ..[#].
-//     [#]. | .#[.]#           ..[#].
-//     old grid                new grid
-//
-// Second loop of iteration:
-//
-//     #[.] | #..[.]  becomes  ..#[.]
-//     #[.] | .#.[#]           ..#[#]
-//     old grid                new grid
-//
-// Finally, store the initial values, this time of the flipped part as it is
-// larger than the unchanged part.
-//
-// First loop of iteration:
-//
-//     #.|[#]...  becomes  [#].#.
-//     #.|[.]#.#           [.].##
-//     old grid            new grid
-//
-// Second loop of iteration:
-//
-//     #.|#[.]..  becomes  #[.]#.
-//     #.|.[#].#           .[#]##
-//     old grid            new grid
-//
-// The result of the fold:
-//
-//     #.#.
-//     .###
-
-func (g *grid) foldX(n int) {
-	// Reverse the flipped part first.
-	for y := 0; y < g.rows; y++ {
-		// Use the fact that g.dots is row-major: create a slice representing
-		// the partial row that we want to reverse, and then reverse only that
-		// slice.
-		start := y*g.cols + (n + 1) // start of row + first column of reversed part
-		end := (y + 1) * g.cols     // start of next row
-		partial := g.dots[start:end]
-		for i, j := 0, len(partial)-1; i < j; i, j = i+1, j-1 {
-			partial[i], partial[j] = partial[j], partial[i]
+func (g *grid) foldX(line int) {
+	// "Fold" to the left by doing the following for each point:
+	//     1. If the point is to the left of the fold line, ignore it.
+	//     2. Calculate the new x-value of the point as
+	//            new_x = old_x - 2*(old_x - line).
+	//        The -1 is because we need to skip over the fold line.
+	for p := range g.points {
+		if p.X < line {
+			continue
 		}
+		p2 := p
+		p2.X = p.X - 2*(p.X-line)
+		delete(g.points, p)
+		g.points[p2] = true
 	}
-
-	// Create a new grid of appropriate size.
-	unchangedCols := n              // Everything up to but not including the fold line.
-	flippedCols := g.cols - (n + 1) // Skip the first n columns, plus the fold line.
-	rows2 := g.rows
-	cols2 := intmath.Max(unchangedCols, flippedCols)
-	dots2 := make([]bool, rows2*cols2)
-	g2 := &grid{
-		dots: dots2,
-		rows: rows2,
-		cols: cols2,
-	}
-
-	// OR together unchanged and flipped part.
-	overlappingCols := intmath.Min(unchangedCols, flippedCols)
-	for y := 0; y < g.rows; y++ {
-		for i := 0; i < overlappingCols; i++ {
-			unchangedX := n - overlappingCols + i    // Relative to fold line.
-			flippedX := g.cols - overlappingCols + i // Relative to end of flipped part.
-			x2 := g2.cols - overlappingCols + i      // Relative to end of new grid.
-			unchangedV, _ := g.Get(unchangedX, y)
-			flippedV, _ := g.Get(flippedX, y)
-			g2.Set(x2, y, unchangedV || flippedV)
-		}
-	}
-
-	// Copy over initial part of either unchanged or flipped part.
-	copyCols := g2.cols - overlappingCols
-	// Assume the unchanged part is bigger -- then we start copying from the
-	// first column.
-	copyStart := 0
-	if flippedCols > unchangedCols {
-		// The flipped part is bigger, so we start copying from 1 step right of
-		// the fold line.
-		copyStart = n + 1
-	}
-	for y := 0; y < g.rows; y++ {
-		for i := 0; i < copyCols; i++ {
-			srcX := copyStart + i
-			dstX := i
-			v, _ := g.Get(srcX, y)
-			g2.Set(dstX, y, v)
-		}
-	}
-
-	// We are done, so overwrite this grid with the result of the fold.
-	*g = *g2
 }
 
-func (g *grid) foldY(n int) {
-	// Reverse the flipped part first.
-	for x := 0; x < g.cols; x++ {
-		for yi, yj := n+1, g.rows-1; yi < yj; yi, yj = yi+1, yj-1 {
-			ii, _ := g.index(x, yi)
-			ij, _ := g.index(x, yj)
-			g.dots[ii], g.dots[ij] = g.dots[ij], g.dots[ii]
+func (g *grid) foldY(line int) {
+	// See the comment in foldX for an explanation of how this works. It is
+	// basically the same but manipulating y-values instead.
+	for p := range g.points {
+		if p.Y < line {
+			continue
 		}
+		p2 := p
+		p2.Y = p.Y - 2*(p.Y-line)
+		delete(g.points, p)
+		g.points[p2] = true
 	}
-
-	// Create a new grid of appropriate size.
-	unchangedRows := n              // Everything up to but not including the fold line.
-	flippedRows := g.rows - (n + 1) // Skip the first n columns, plus the fold line.
-	rows2 := intmath.Max(unchangedRows, flippedRows)
-	cols2 := g.cols
-	dots2 := make([]bool, rows2*cols2)
-	g2 := &grid{
-		dots: dots2,
-		rows: rows2,
-		cols: cols2,
-	}
-
-	// OR together unchanged and flipped part.
-	overlappingRows := intmath.Min(unchangedRows, flippedRows)
-	for x := 0; x < g.cols; x++ {
-		for i := 0; i < overlappingRows; i++ {
-			unchangedY := n - overlappingRows + i    // Relative to fold line.
-			flippedY := g.rows - overlappingRows + i // Relative to end of flipped part.
-			y2 := g2.rows - overlappingRows + i      // Relative to end of new grid.
-			unchangedV, _ := g.Get(x, unchangedY)
-			flippedV, _ := g.Get(x, flippedY)
-			g2.Set(x, y2, unchangedV || flippedV)
-		}
-	}
-
-	// Copy over initial part of either unchanged or flipped part.
-	copyRows := g2.rows - overlappingRows
-	// Assume the unchanged part is bigger -- then we start copying from the
-	// first row.
-	copyStart := 0
-	if flippedRows > unchangedRows {
-		// The flipped part is bigger, so we start copying from 1 step below the
-		// fold line.
-		copyStart = n + 1
-	}
-	for x := 0; x < g.cols; x++ {
-		for i := 0; i < copyRows; i++ {
-			srcY := copyStart + i
-			dstY := i
-			v, _ := g.Get(x, srcY)
-			g2.Set(x, dstY, v)
-		}
-	}
-
-	// We are done, so overwrite this grid with the result of the fold.
-	*g = *g2
 }
