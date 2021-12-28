352e44641e8f375a8c692fb3997ec6f33a2a68d7
Christian Persson <Saser@users.noreply.github.com>
2021-12-28 20:12:28 +0000

    adventofcode/go: 2021/18: add a solution using a simple list of regular numbers.
    I think I was pretty close to figuring this out in my previous attempt, but
    didn't quite. A lot of this solution is inspired by the solution here:
    https://github.com/89netraM/Advent-of-Code/blob/8e470f1a697247a9ce1ca4725e17d6c2dc8d5400/2021/18/Day18.rs
    
    Benchmark comparison to my old solution:
    
        name             old time/op  new time/op  delta
        Part1/actual-16  1.81ms ± 7%  0.06ms ± 6%  -96.63%  (p=0.000 n=10+10)
        Part2/actual-16  51.7ms ± 8%   7.8ms ±11%  -84.87%  (p=0.000 n=10+10)
    
    Quite the improvement!

--- adventofcode/go/year2021/day18/day18.go
+++ adventofcode/go/year2021/day18/day18.go
@@ -1,30 +1,7 @@
 package day18
 
-/*
-Some ideas on how this could maybe be made faster, based on what I've found when
-profiling.
-
-1. Use some kind of queue (maybe a priority queue) for pairs that can explode.
-   Most of the time in Explode is spent looking for a pair to explode (if any).
-	- In Add, clear and reinitialize the queue after joining the two trees.
-	  Depths need to be recalculated so the contents of the queue will probably
-	  change.
-	- In Explode, pop the first item from the queue (if any), otherwise return
-	  early.
-	- In Split, check the depth of the just created pair, and add it to the
-	  queue. The difficult part is figuring out where in the queue it should be
-	  added.
-
-2. Use a priority queue for regular numbers that can be split. All >=10 numbers
-   are prioritized over all <10 numbers, and among >=10 numbers the
-   left-to-right order decides priority. The difficult part is how to determine
-   this order. One idea could be to assign them left-to-right indices in init()
-   and then use that in the priority queue implementation.
-*/
-
 import (
 	"fmt"
-	"strconv"
 	"strings"
 
 	"github.com/Saser/pdp/adventofcode/go/intmath"
@@ -39,10 +16,23 @@ func Part2(input string) (string, error) {
 }
 
 func solve(input string, part int) (string, error) {
-	numbers := parse(input)
+	lines := strings.Split(strings.TrimSpace(input), "\n")
 	if part == 1 {
-		result := sumAll(numbers)
+		depth := 0
+		result := newNumber()
+		result.Parse(lines[0], depth)
+		for _, line := range lines[1:] {
+			result.Parse(line, depth)
+			result.Reduce(depth - 1)
+			depth--
+		}
 		return fmt.Sprint(result.Magnitude()), nil
+	}
+	numbers := make([]*number, len(lines))
+	for i, line := range lines {
+		n := newNumber()
+		n.Parse(line, 0)
+		numbers[i] = n
 	}
 	max := 0
 	for _, n1 := range numbers {
@@ -50,373 +40,201 @@ func solve(input string, part int) (string, error) {
 			if n1 == n2 {
 				continue
 			}
-			result := n1.Clone()
-			result.Add(n2.Clone())
-			result.Reduce()
+			result := n1.Copy()
+			result.Add(n2)
+			result.Reduce(-1)
 			max = intmath.Max(max, result.Magnitude())
 		}
 	}
 	return fmt.Sprint(max), nil
 }
 
-func sumAll(numbers []*number) *number {
-	result := numbers[0]
-	for _, other := range numbers[1:] {
-		result.Add(other)
-		result.Reduce()
+type regular struct {
+	Value int
+	Depth int
+}
+
+type number []regular
+
+func newNumber() *number {
+	// I made a guess that each line contains at most 32 regular numbers. By
+	// allocating once for twice that amount, we can add numbers by parsing them
+	// into the same slice and avoid reallocating, saving memory and runtime.
+	const maxRegulars = 32
+	regulars := make([]regular, 0, 2*maxRegulars)
+	return (*number)(&regulars)
+}
+
+func (n *number) Parse(s string, depth int) {
+	for _, r := range s {
+		switch r {
+		case '[':
+			depth++
+		case ']':
+			depth--
+		case ',':
+			// Do nothing.
+		default:
+			*n = append(*n, regular{
+				Value: int(r - '0'),
+				Depth: depth,
+			})
+		}
 	}
-	return result
 }
 
-func parse(input string) []*number {
-	var numbers []*number
-	for _, line := range strings.Split(strings.TrimSpace(input), "\n") {
-		numbers = append(numbers, parseNumber(line))
-	}
-	return numbers
-}
-
-func parseNumber(line string) *number {
-	return makeNumber(parseNode(line))
-}
-
-func parseNode(s string) *node {
-	n, rem := parseNodeAux(s)
-	if rem != "" {
-		panic(fmt.Errorf("parsing %q returned non-empty remainder %q", s, rem))
-	}
-	return n
-}
-
-func parseNodeAux(s string) (*node, string) {
-	if s[0] == '[' {
-		left, rem := parseNodeAux(s[1:])    // skip the '['
-		right, rem := parseNodeAux(rem[1:]) // skip the ','
-		rem = rem[1:]                       // skip the ']'
-		return &node{
-			Left:  left,
-			Right: right,
-		}, rem
-	}
-	var (
-		v   int
-		rem string
-		err error
-	)
-	end := strings.IndexAny(s, "[],")
-	if end == -1 {
-		v, err = strconv.Atoi(s)
-		rem = ""
-	} else {
-		v, err = strconv.Atoi(s[:end])
-		rem = s[end:]
-	}
-	if err != nil {
-		panic(err)
-	}
-	return &node{
-		Value: v,
-	}, rem
-}
-
-func makeNumber(root *node) *number {
-	n := &number{root: root}
-	n.init()
-	return n
-}
-
-func assignElements(n *number, nd *node) {
-	if nd.regular {
-		n.pushBack(nd)
-		return
-	}
-	assignElements(n, nd.Left)
-	assignElements(n, nd.Right)
-}
-
-type number struct {
-	root *node
-
-	front, back *node
-}
-
-func (n *number) init() {
-	n.front = nil
-	n.back = nil
-	n.root.init(0)
-	n.assignLinks(n.root)
-}
-
-func (n *number) assignLinks(nd *node) {
-	if nd.regular {
-		n.pushBack(nd)
-		return
-	}
-	n.assignLinks(nd.Left)
-	n.assignLinks(nd.Right)
-}
-
-func (n *number) Clone() *number {
-	n2 := &number{root: n.root.clone()}
-	n2.init()
-	return n2
-}
-
-func (n *number) String() string {
-	return n.root.String() + "\n" + n.ListString()
-}
-
-func (n *number) ListString() string {
-	var rs []string
-	for r := n.front; r != nil; r = r.next {
-		rs = append(rs, fmt.Sprint(r.Value))
-	}
-	return "[" + strings.Join(rs, ",") + "]"
-}
-
-func (n *number) Magnitude() int {
-	return n.root.magnitude()
+func (n *number) Copy() *number {
+	n2 := make([]regular, len(*n))
+	copy(n2, *n)
+	return (*number)(&n2)
 }
 
 func (n *number) Add(other *number) {
-	// Create a new root by setting n to the left and other to the right
-	// elements.
-	root := &node{
-		Parent: nil,
-		Left:   n.root,
-		Right:  other.root,
-	}
-	root.Left.Parent = root
-	root.Right.Parent = root
-	root.init(0)
-	n.root = root
-	// Join n's linked list of regular numbers with other's list.
-	n.back.next = other.front
-	other.front.prev = n.back
-	n.back = other.back
-	// Other shouldn't be used after calling Add, but if it is used (due to a
-	// bug), fail loudly by panicking due to nil derefs.
-	other.root = nil
-	other.front = nil
-	other.back = nil
+	*n = append(*n, (*other)...)
 }
 
-func (n *number) Reduce() {
-	for {
-		if changed := n.reduceStep(); !changed {
-			break
+func (n *number) Reduce(depth int) {
+	// First make sure all pairs that can explode have done so. We can do that
+	// in one iteration over the list of regular numbers, using the fact that no
+	// regular number will have a depth of more than 5 [*]. A pair that explodes
+	// is replaced by a regular number which will be part of a pair which will
+	// never be nested inside 4 other pairs. So we can just explode one pair and
+	// then move on.
+	//
+	// [*]: The instructions say that all exploding pairs will only contain
+	// regular numbers. If there was a regular number of a depth of more than 5,
+	// then it would be part of a pair that is nested inside more than 4 other
+	// pairs.  But then there would be some pair nested inside exactly 4 other
+	// pairs which would have non-regular numbers as children -- which the
+	// instructions say cannot happen.
+	for i := 0; i < len(*n); i++ {
+		r := &(*n)[i]
+		if r.Depth-depth != 5 {
+			continue
+		}
+		// r is the left number in an exploding pair. Add its value to the
+		// previous number (if any), add the next number (which is the right
+		// number in the exploding pair) to its subsequent number (if any),
+		// and then replace the left and right numbers with a single 0 at a
+		// depth of one less than the exploding numbers.
+		left := r
+		right := (*n)[i+1]
+		if prev := i - 1; prev >= 0 {
+			(*n)[prev].Value += left.Value
+		}
+		if next := i + 2; next < len(*n) {
+			(*n)[next].Value += right.Value
+		}
+		r.Value = 0
+		r.Depth--
+		*n = append((*n)[:i+1], (*n)[i+2:]...)
+	}
+
+	// Now we know that the next action (if any) will be a split. Splitting
+	// could result in new pairs that should explode, but these pairs will be
+	// created in the same place as the number that is split, so we can do a
+	// split and the resulting explosion at once. However, the explosion could
+	// mean that a number we skipped over should be split. Example:
+	//     Initially: [[[[9, 10], 8], ...]]
+	//     Split:     [[[[9, [5,5]], 8], ...]]
+	//     Explode:   [[[[14, 0], 13], ...]]
+	// So whenever we split-and-explode, we have to take one step back (if
+	// possible).
+	i := 0
+	for i < len(*n) {
+		r := &(*n)[i]
+		if r.Value < 10 {
+			i++
+			continue
+		}
+		lo := r.Value / 2
+		hi := r.Value/2 + r.Value%2
+		if r.Depth-depth == 4 {
+			// The split number is at depth 5, which will create a pair at
+			// depth 5, which should then explode. We do the explosion by
+			// simply going to the previos and next numbers (if any) and
+			// adding lo and hi. Then the split number is replaced by a 0.
+			if prev := i - 1; prev >= 0 {
+				(*n)[prev].Value += lo
+			}
+			if next := i + 1; next < len(*n) {
+				(*n)[next].Value += hi
+			}
+			r.Value = 0
+			if i > 0 {
+				i-- // Take one step back, as explained above.
+			} else {
+				i++
+			}
+		} else {
+			// The split number results in a pair that doesn't immediately
+			// explode. We represent this by replacing the split number with lo,
+			// adding a new regular number with hi, and set both of these
+			// numbers' depth to the split number depth plus one.
+			r.Value = lo
+			r.Depth++
+			r2 := regular{
+				Value: hi,
+				Depth: r.Depth,
+			}
+			*n = append((*n)[:i+1], append([]regular{r2}, (*n)[i+1:]...)...)
+			// This check seemed weird to me at first, but it's entirely
+			// possible that a series of splits and explosions leads to a bunch
+			// of larger-than-10 values accumulate close to each other. When
+			// these values are split and exploded, some may end up being 20 or
+			// larger, and therefore needs several splits. So we can only move
+			// on to the next regular number if the new value of r is less than
+			// 10.
+			if r.Value < 10 {
+				i++
+			}
 		}
 	}
 }
 
-func (n *number) reduceStep() bool {
-	if n.Explode() {
-		return true
-	}
-	if n.Split() {
-		return true
-	}
-	return false
-}
-
-func (n *number) Explode() bool {
-	var exploding *node
-	for r := n.front; r != nil; r = r.next {
-		if parent := r.Parent; parent.Left.regular && parent.Right.regular && parent.depth == 4 {
-			exploding = parent
-			break
+func (n *number) Magnitude() int {
+	var stack []regular
+	for _, r := range *n {
+		stack = append(stack, r)
+		for len(stack) >= 2 {
+			left := stack[len(stack)-2]
+			right := stack[len(stack)-1]
+			if left.Depth != right.Depth {
+				break
+			}
+			stack = stack[:len(stack)-2]
+			stack = append(stack, regular{
+				Value: 3*left.Value + 2*right.Value,
+				Depth: left.Depth - 1,
+			})
 		}
 	}
-	if exploding == nil {
-		return false
-	}
-	// Now that we have found the exploding pair, increase the values of the
-	// previous and next regular numbers.
-	left := exploding.Left
-	right := exploding.Right
-	if prev := left.prev; prev != nil {
-		prev.Value += left.Value
-	}
-	if next := right.next; next != nil {
-		next.Value += right.Value
-	}
-	// After increasing the surrounding values, we replace the exploding pair
-	// with a regular 0.
-	exploding.Value = 0
-	exploding.Left = nil
-	exploding.Right = nil
-	exploding.regular = true
-	// Now we can remove the left and right regular numbers from the linked
-	// list, and insert the 0 in their place. We do this by placing the 0
-	// between them and then removing them. Why between? To make the code
-	// shorter by not dealing with edge cases at the front or back of the list.
-	n.insertAfter(exploding, left)
-	n.remove(left)
-	n.remove(right)
-	return true
+	return stack[0].Value
 }
 
-func (n *number) Split() bool {
-	var split *node
-	for r := n.front; r != nil; r = r.next {
-		if r.Value >= 10 {
-			split = r
-			break
+func (n *number) String() string {
+	type item struct {
+		depth int
+		s     string
+	}
+	var stack []item
+	for _, r := range *n {
+		stack = append(stack, item{
+			depth: r.Depth,
+			s:     fmt.Sprint(r.Value),
+		})
+		for len(stack) >= 2 {
+			left := stack[len(stack)-2]
+			right := stack[len(stack)-1]
+			if left.depth != right.depth {
+				break
+			}
+			stack = stack[:len(stack)-2]
+			stack = append(stack, item{
+				depth: left.depth - 1,
+				s:     "[" + left.s + "," + right.s + "]",
+			})
 		}
 	}
-	if split == nil {
-		return false
-	}
-	// Replace the split node with a pair.
-	split.Left = &node{
-		Parent:  split,
-		Value:   split.Value / 2,
-		regular: true,
-		depth:   split.depth + 1,
-	}
-	split.Right = &node{
-		Parent:  split,
-		Value:   split.Value/2 + split.Value%2,
-		regular: true,
-		depth:   split.depth + 1,
-	}
-	split.Value = 0
-	split.regular = false
-	// Remove the split node from the linked list and insert the two new
-	// elements in its place. Do this by placing the new elements around the
-	// split node and then remove the split node. Why around? To make the code
-	// shorter by not having to deal with edge cases where split is at the front
-	// or back of the list.
-	n.insertBefore(split.Left, split)
-	n.insertAfter(split.Right, split)
-	n.remove(split)
-	return true
-}
-
-func (n *number) pushBack(r *node) {
-	if n.back == nil {
-		r.prev = nil
-		r.next = nil
-		n.front = r
-		n.back = r
-		return
-	}
-	n.back.next = r
-	r.prev = n.back
-	r.next = nil
-	n.back = r
-}
-
-func (n *number) insertBefore(r, mark *node) {
-	if mark == n.front {
-		r.prev = nil
-		r.next = mark
-		mark.prev = r
-		n.front = r
-		return
-	}
-	r.prev = mark.prev
-	r.next = mark
-	mark.prev.next = r
-	mark.prev = r
-}
-
-func (n *number) insertAfter(r, mark *node) {
-	if mark == n.back {
-		r.prev = mark
-		r.next = nil
-		mark.next = r
-		n.back = r
-		return
-	}
-	r.prev = mark
-	r.next = mark.next
-	mark.next.prev = r
-	mark.next = r
-}
-
-func (n *number) remove(mark *node) {
-	if mark == n.front && mark == n.back {
-		n.front = nil
-		n.back = nil
-		mark.prev = nil
-		mark.next = nil
-		return
-	}
-	if mark == n.front {
-		n.front = mark.next
-		mark.next.prev = nil
-		mark.prev = nil
-		mark.next = nil
-		return
-	}
-	if mark == n.back {
-		n.back = mark.prev
-		mark.prev.next = nil
-		mark.prev = nil
-		mark.next = nil
-		return
-	}
-	mark.prev.next = mark.next
-	mark.next.prev = mark.prev
-	mark.prev = nil
-	mark.next = nil
-}
-
-type node struct {
-	Parent *node
-
-	// These are either both nil or both non-nil. If both are nil, then this is
-	// a regular number. If both are non-nil, then this is a pair.
-	Left, Right *node
-
-	// These fields are only valid if this is a regular number.
-	Value int
-	// The previous and next nodes in the linked list of regular numbers. These
-	// fields can be nil, and are always updated by the operations on number
-	// (Add, pushBack, insertBefore, insertAfter, remove).
-	prev, next *node
-
-	regular bool
-	depth   int
-}
-
-func (n *node) pair() bool {
-	return n.Left != nil && n.Right != nil
-}
-
-func (n *node) magnitude() int {
-	if n.regular {
-		return n.Value
-	}
-	return 3*n.Left.magnitude() + 2*n.Right.magnitude()
-}
-
-func (n *node) init(depth int) {
-	n.depth = depth
-	if n.Left == nil && n.Right == nil {
-		n.regular = true
-		return
-	}
-	n.Left.Parent = n
-	n.Right.Parent = n
-	n.Left.init(depth + 1)
-	n.Right.init(depth + 1)
-}
-
-func (n *node) clone() *node {
-	if n.regular {
-		return &node{Value: n.Value}
-	}
-	return &node{
-		Left:  n.Left.clone(),
-		Right: n.Right.clone(),
-	}
-}
-
-func (n *node) String() string {
-	if n.regular {
-		return fmt.Sprint(n.Value)
-	}
-	return "[" + n.Left.String() + "," + n.Right.String() + "]"
+	return stack[0].s
 }
--- adventofcode/go/year2021/day18/day18_test.go
+++ adventofcode/go/year2021/day18/day18_test.go
@@ -1,89 +1,39 @@
 package day18
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
-func equalNumber(n1, n2 *number) bool {
-	if n1.root.String() != n2.root.String() {
-		return false
-	}
-	var rs1 []string
-	for r := n1.front; r != nil; r = r.next {
-		rs1 = append(rs1, fmt.Sprint(r.Value))
-	}
-	var rs2 []string
-	for r := n2.front; r != nil; r = r.next {
-		rs2 = append(rs2, fmt.Sprint(r.Value))
-	}
-	return strings.Join(rs1, ",") == strings.Join(rs2, ",")
+func parseNumber(s string, depth int) *number {
+	n := newNumber()
+	n.Parse(s, depth)
+	return n
 }
 
-func TestParseNumber(t *testing.T) {
-	// p is a shorthand for constructing a pair. Left and right must either be
-	// an int or a *pair.
-	p := func(left, right any) *node {
-		p := &node{}
-		switch v := left.(type) {
-		case int:
-			p.Left = &node{
-				Value: v,
-			}
-		case *node:
-			p.Left = v
-		}
-		switch v := right.(type) {
-		case int:
-			p.Right = &node{
-				Value: v,
-			}
-		case *node:
-			p.Right = v
-		}
-		return p
-	}
+func TestNumber_Reduce(t *testing.T) {
+	const depth = 0
 	for _, tt := range []struct {
-		line string
-		want *node
+		n    *number
+		want *number
 	}{
 		{
-			line: "[1,2]",
-			want: p(1, 2),
+			n:    parseNumber("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]", depth),
+			want: parseNumber("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", depth),
 		},
 		{
-			line: "[[1,2],3]",
-			want: p(p(1, 2), 3),
-		},
-		{
-			line: "[9,[8,7]]",
-			want: p(9, p(8, 7)),
-		},
-		{
-			line: "[[1,9],[8,5]]",
-			want: p(p(1, 9), p(8, 5)),
-		},
-		{
-			line: "[[[[1,2],[3,4]],[[5,6],[7,8]]],9]",
-			want: p(p(p(p(1, 2), p(3, 4)), p(p(5, 6), p(7, 8))), 9),
-		},
-		{
-			line: "[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]",
-			want: p(p(p(9, p(3, 8)), p(p(0, 9), 6)), p(p(p(3, 7), p(4, 9)), 3)),
-		},
-		{
-			line: "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]",
-			want: p(p(p(p(1, 3), p(5, 3)), p(p(1, 3), p(8, 7))), p(p(p(4, 9), p(6, 9)), p(p(8, 2), p(7, 3)))),
+			n:    parseNumber("[[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]],[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]]", depth),
+			want: parseNumber("[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]", depth),
 		},
 	} {
-		got := parseNode(tt.line)
-		if diff := cmp.Diff(tt.want, got, cmpopts.IgnoreUnexported(node{})); diff != "" {
-			t.Errorf("unexpected result from parsing (-want +got)\n%s", diff)
-		}
+		t.Run(tt.n.String(), func(t *testing.T) {
+			tt.n.Reduce(depth)
+			got := tt.n // tt.n modified in-place
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("Reduce(): unexpected result (-want +got)\n%s", diff)
+			}
+		})
 	}
 }
 
@@ -93,146 +43,46 @@ func TestNumber_Magnitude(t *testing.T) {
 		want int
 	}{
 		{
-			n:    parseNumber("10"),
-			want: 10,
-		},
-		{
-			n:    parseNumber("[9,1]"),
+			n:    parseNumber("[9,1]", 0),
 			want: 29,
 		},
 		{
-			n:    parseNumber("[[9,1],[1,9]]"),
+			n:    parseNumber("[1,9]", 0),
+			want: 21,
+		},
+		{
+			n:    parseNumber("[[9,1],[1,9]]", 0),
 			want: 129,
 		},
 		{
-			n:    parseNumber("[[1,2],[[3,4],5]]"),
+			n:    parseNumber("[[1,2],[[3,4],5]]", 0),
 			want: 143,
 		},
 		{
-			n:    parseNumber("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"),
+			n:    parseNumber("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", 0),
 			want: 1384,
 		},
 		{
-			n:    parseNumber("[[[[1,1],[2,2]],[3,3]],[4,4]]"),
+			n:    parseNumber("[[[[1,1],[2,2]],[3,3]],[4,4]]", 0),
 			want: 445,
 		},
 		{
-			n:    parseNumber("[[[[3,0],[5,3]],[4,4]],[5,5]]"),
+			n:    parseNumber("[[[[3,0],[5,3]],[4,4]],[5,5]]", 0),
 			want: 791,
 		},
 		{
-			n:    parseNumber("[[[[5,0],[7,4]],[5,5]],[6,6]]"),
+			n:    parseNumber("[[[[5,0],[7,4]],[5,5]],[6,6]]", 0),
 			want: 1137,
 		},
 		{
-			n:    parseNumber("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"),
+			n:    parseNumber("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 0),
 			want: 3488,
 		},
 	} {
-		if got := tt.n.Magnitude(); got != tt.want {
-			t.Errorf("Magnitude() = %v; want %v", got, tt.want)
-		}
-	}
-}
-
-func TestNumber_Add(t *testing.T) {
-	for _, tt := range []struct {
-		n     *number
-		other *number
-		want  *number
-	}{
-		{
-			n:     parseNumber("[[[[4,3],4],4],[7,[[8,4],9]]]"),
-			other: parseNumber("[1,1]"),
-			want:  parseNumber("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"),
-		},
-	} {
-		tt.n.Add(tt.other)
-		got := tt.n // Add() modified the receiver in-place
-		if diff := cmp.Diff(tt.want, got, cmp.Comparer(equalNumber)); diff != "" {
-			t.Errorf("unexpected result from Add() (-want +got)\n%s", diff)
-		}
-	}
-}
-
-func TestNumber_Reduce(t *testing.T) {
-	for _, tt := range []struct {
-		n    *number
-		want *number
-	}{
-		{
-			n:    parseNumber("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"),
-			want: parseNumber("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"),
-		},
-	} {
-		tt.n.Reduce()
-		got := tt.n // Reduce() modified tt.n in-place
-		if diff := cmp.Diff(tt.want, got, cmp.Comparer(equalNumber)); diff != "" {
-			t.Errorf("unexpected result of Reduce() (-want +got)\n%s", diff)
-		}
-	}
-}
-
-func TestNumber_Explode(t *testing.T) {
-	for _, tt := range []struct {
-		n    *number
-		want *number
-	}{
-		{
-			n:    parseNumber("[[[[[9,8],1],2],3],4]"),
-			want: parseNumber("[[[[0,9],2],3],4]"),
-		},
-		{
-			n:    parseNumber("[7,[6,[5,[4,[3,2]]]]]"),
-			want: parseNumber("[7,[6,[5,[7,0]]]]"),
-		},
-		{
-			n:    parseNumber("[[6,[5,[4,[3,2]]]],1]"),
-			want: parseNumber("[[6,[5,[7,0]]],3]"),
-		},
-		{
-			n:    parseNumber("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"),
-			want: parseNumber("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"),
-		},
-		{
-			n:    parseNumber("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"),
-			want: parseNumber("[[3,[2,[8,0]]],[9,[5,[7,0]]]]"),
-		},
-	} {
-		t.Run(tt.n.root.String(), func(t *testing.T) {
-			changed := tt.n.Explode()
-			if got, want := changed, true; got != want {
-				t.Fatalf("Explode() = %v; want %v", got, want)
-			}
-			got := tt.n // Explode() modified tt.n in-place
-			if diff := cmp.Diff(tt.want, got, cmp.Comparer(equalNumber)); diff != "" {
-				t.Errorf("unexpected result of Explode() (-want +got)\n%s", diff)
+		t.Run(tt.n.String(), func(t *testing.T) {
+			if got := tt.n.Magnitude(); got != tt.want {
+				t.Errorf("Magnitude() = %v; want %v", got, tt.want)
 			}
 		})
-	}
-}
-
-func TestNumber_Split(t *testing.T) {
-	for _, tt := range []struct {
-		n    *number
-		want *number
-	}{
-		{
-			n:    parseNumber("[[[[0,7],4],[15,[0,13]]],[1,1]]"),
-			want: parseNumber("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"),
-		},
-		{
-			n:    parseNumber("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"),
-			want: parseNumber("[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]"),
-		},
-	} {
-		changed := tt.n.Split()
-		if got, want := changed, true; got != want {
-			t.Fatalf("Split() = %v; want %v", got, want)
-		}
-		got := tt.n // Split() modified tt.n in-place
-		if diff := cmp.Diff(tt.want, got, cmp.Comparer(equalNumber)); diff != "" {
-			t.Errorf("unexpected result of Split() (-want +got)\n%s", diff)
-		}
 	}
 }
